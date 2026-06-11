924942cba44eb858744bcfd032f6e664de9aa7d1
Cynthia Villagra <amandaodoo@gmail.com>
2026-06-11 01:17:02 -0300

    primer commit

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ---
-title: Gradio
+title: App Prueba G
 emoji: 💻
-colorFrom: yellow
-colorTo: gray
+colorFrom: pink
+colorTo: green
 sdk: gradio
 sdk_version: 6.17.3
-python_version: '3.13'
+python_version: '3.10'
 app_file: app.py
 pinned: false
 short_description: gradio
---
+++ app.py
@@ -0,0 +1,14 @@
+import gradio as gr
+
+def saludar(nombre):
+    return f"Hola, {nombre}!"
+
+demo = gr.Interface(
+    fn=saludar,
+    inputs="text",
+    outputs="text",
+    title="App basica con Gradio",
+    description="Escribi tu nombre y la app te saluda."
+)
+
+demo.launch()
